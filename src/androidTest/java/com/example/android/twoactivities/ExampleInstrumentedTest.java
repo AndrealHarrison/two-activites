3cab14b78491d458488bf3b3bd598fbb772223e3
Andrea Harrison <Andrealharrison@gmail.com>
2022-02-25 13:06:45 -0800

    initial

--- src/androidTest/java/com/example/android/twoactivities/ExampleInstrumentedTest.java
+++ src/androidTest/java/com/example/android/twoactivities/ExampleInstrumentedTest.java
@@ -32,6 +32,8 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
